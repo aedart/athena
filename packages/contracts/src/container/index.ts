d53efbd605ab821e419483f0d7ea6ebc6f7cfec8
Alin Eugen Deac <aedart@gmail.com>
2021-08-21 15:22:21 +0200

    Export container interface and identifier

--- packages/contracts/src/container/index.ts
+++ packages/contracts/src/container/index.ts
@@ -1,6 +1,14 @@
+import ContainerException from "./exceptions/ContainerException";
+import BindingException from "./exceptions/BindingException";
+import NotFoundException from "./exceptions/NotFoundException";
+import Binding from "./entries/Binding";
 import Container, { CONTAINER } from "./Container";
 
 export {
+    ContainerException,
+    BindingException,
+    NotFoundException,
+    Binding,
     CONTAINER,
     Container as default
 };

42e5393b057c7ea69b54d9dbe254d94d6a14620b
Alin Eugen Deac <aedart@gmail.com>
2021-08-21 16:34:14 +0200

    Add binding implementation

---
+++ packages/container/src/entries/Binding.ts
@@ -0,0 +1,113 @@
+import {
+    Binding as BindingContract,
+    BindingIdentifier,
+    ConcreteCallback,
+    ConcreteInstance
+} from "@aedart/contracts/dist/container";
+
+/**
+ * Binding
+ *
+ * @see BindingContract
+ */
+export default class Binding implements BindingContract {
+
+    /**
+     * The abstract abstractIdentifier of this binding
+     *
+     * @protected
+     *
+     * @type {BindingIdentifier}
+     */
+    protected readonly abstractIdentifier: BindingIdentifier;
+
+    /**
+     * Concrete value of binding
+     *
+     * @protected
+     *
+     * @type {ConcreteCallback|ConcreteInstance}
+     */
+    protected readonly concreteValue: ConcreteCallback | ConcreteInstance;
+
+    /**
+     * State whether binding is shared or not
+     *
+     * @protected
+     *
+     * @type {boolean}
+     */
+    protected readonly isShared: boolean;
+
+    /**
+     * State whether binding's concrete value is of type callback
+     * or not
+     *
+     * @protected
+     *
+     * @type {boolean}
+     */
+    protected readonly isOfTypeCallback: boolean;
+
+    /**
+     * Binding
+     *
+     * @param {BindingIdentifier} abstract
+     * @param {ConcreteCallback|ConcreteInstance} [concrete]
+     * @param {boolean} [shared]
+     * @param {boolean} [isCallback]
+     */
+    constructor(
+        abstract: BindingIdentifier,
+        concrete: ConcreteCallback | ConcreteInstance = null,
+        shared: boolean = false,
+        isCallback: boolean = false
+    ) {
+        this.abstractIdentifier = abstract;
+        this.concreteValue = concrete;
+        this.isShared = shared;
+        this.isOfTypeCallback = isCallback;
+    }
+
+    /**
+     * Returns the binding abstractIdentifier
+     *
+     * @return {BindingIdentifier}
+     */
+    get abstract(): BindingIdentifier {
+        return this.abstractIdentifier;
+    }
+
+    /**
+     * Returns the concrete instance or callback associated
+     * with the binding
+     *
+     * @return {ConcreteCallback|ConcreteInstance}
+     */
+    get concrete(): ConcreteCallback | ConcreteInstance {
+        return this.concreteValue;
+    }
+
+    /**
+     * Whether the concrete instance is a callback
+     * that must be invoked by the container, to resolve
+     * the concrete instance.
+     *
+     * @return {boolean}
+     */
+    get isCallback(): boolean {
+        return this.isOfTypeCallback;
+    }
+
+    /**
+     * Whether this binding acts as a shared or not.
+     *
+     * A shared binding acts as a singleton; this same
+     * concrete instance is returned from the container
+     *
+     * @return {boolean}
+     */
+    get shared(): boolean {
+        return this.isShared;
+    }
+}
--- packages/container/src/index.ts
+++ packages/container/src/index.ts
@@ -1,9 +1,11 @@
 import ContainerException from "./exceptions/ContainerException";
 import NotFoundException from "./exceptions/NotFoundException";
 import BindingResolutionException from "./exceptions/BindingResolutionException";
+import Binding from "./entries/Binding";
 
 export {
     ContainerException,
     NotFoundException,
-    BindingResolutionException
+    BindingResolutionException,
+    Binding
 }

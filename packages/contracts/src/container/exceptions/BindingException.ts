85cbdd3ba567f1790ea86301da054e35e2889967
Alin Eugen Deac <aedart@gmail.com>
2021-08-21 14:08:14 +0200

    Add custom container related exceptions

---
+++ packages/contracts/src/container/exceptions/BindingException.ts
@@ -0,0 +1,10 @@
+import ContainerException from "./ContainerException";
+
+/**
+ * Binding Exception
+ *
+ * Should be thrown whenever a binding is invalid or unable to be resolved
+ */
+export default interface BindingException extends ContainerException {
+
+}
---
+++ packages/contracts/src/container/exceptions/ContainerException.ts
@@ -0,0 +1,8 @@
+/**
+ * Container Exception
+ *
+ * A general exception for container related exceptions
+ */
+export default interface ContainerException extends Error {
+
+}

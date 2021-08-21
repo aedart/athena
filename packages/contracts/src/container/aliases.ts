abc8b4e0e02cbad73dd7d792116658dbdde97729
Alin Eugen Deac <aedart@gmail.com>
2021-08-21 13:51:08 +0200

    Add binding interface

---
+++ packages/contracts/src/container/aliases.ts
@@ -0,0 +1,24 @@
+import Container from "./Container";
+
+/**
+ * A binding identifier (also known as "abstract") can consist
+ * of several types; a string, a symbol or perhaps a function.
+ *
+ * It is commonly paired with a "concrete" object instance,
+ * function or other type of value.
+ *
+ * @see ConcreteInstance
+ */
+export type BindingIdentifier = string | Function | symbol | object;
+
+/**
+ * The concrete instance of a binding
+ *
+ * @see BindingIdentifier
+ */
+export type ConcreteInstance = any;
+
+/**
+ * A callback method that must return a concrete instance
+ */
+export type ConcreteCallback = (container: Container, ...params: any[]) => ConcreteInstance;
---
+++ packages/contracts/src/container/entries/Binding.ts
@@ -0,0 +1,42 @@
+import {
+    BindingIdentifier,
+    ConcreteCallback,
+    ConcreteInstance
+} from "../aliases";
+
+/**
+ * Binding identifier
+ */
+export const BINDING: unique symbol = Symbol('@aedart/contracts/container/binding');
+
+/**
+ * Binding
+ */
+export default interface Binding {
+
+    /**
+     * Returns the binding identifier
+     */
+    get abstract(): BindingIdentifier;
+
+    /**
+     * Returns the concrete instance or callback associated
+     * with the binding
+     */
+    get concrete(): ConcreteCallback | ConcreteInstance;
+
+    /**
+     * Whether the concrete instance is a callback
+     * that must be invoked by the container, to resolve
+     * the concrete instance.
+     */
+    get isCallback(): boolean;
+
+    /**
+     * Whether this binding acts as a shared or not.
+     *
+     * A shared binding acts as a singleton; this same
+     * concrete instance is returned from the container
+     */
+    get shared(): boolean;
+}

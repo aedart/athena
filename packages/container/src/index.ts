9071e7e9433cf9fa6eae3662624e0df4f4c1dd52
Alin Eugen Deac <aedart@gmail.com>
2021-08-21 12:46:31 +0200

    Init container package

---
+++ packages/container/src/index.ts
@@ -0,0 +1,12 @@
+import ContainerContract, { CONTAINER } from "@aedart/contracts/dist/container";
+
+/**
+ * TODO: ...
+ */
+export default class Container implements ContainerContract {
+
+    todo() {
+        return CONTAINER;
+    }
+
+}

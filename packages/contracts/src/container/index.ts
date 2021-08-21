ee5984a5c0b3e5422c13c807e50a8c3ee538bc33
Alin Eugen Deac <aedart@gmail.com>
2021-08-21 15:55:59 +0200

    Export type aliases

--- packages/contracts/src/container/index.ts
+++ packages/contracts/src/container/index.ts
@@ -3,12 +3,20 @@ import BindingException from "./exceptions/BindingException";
 import NotFoundException from "./exceptions/NotFoundException";
 import Binding from "./entries/Binding";
 import Container, { CONTAINER } from "./Container";
+import {
+    BindingIdentifier,
+    ConcreteCallback,
+    ConcreteInstance
+} from "./aliases";
 
 export {
     ContainerException,
     BindingException,
     NotFoundException,
     Binding,
+    BindingIdentifier,
+    ConcreteCallback,
+    ConcreteInstance,
     CONTAINER,
     Container as default
 };

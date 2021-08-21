55763d97b5bd1bb7a8fbc19a550f313acb3140b1
Alin Eugen Deac <aedart@gmail.com>
2021-08-21 14:12:34 +0200

    Add not found exception
    Inspired by PSR-11

---
+++ packages/contracts/src/container/exceptions/NotFoundException.ts
@@ -0,0 +1,10 @@
+import ContainerException from "./ContainerException";
+
+/**
+ * Not Found Exception
+ *
+ * Should be thrown when an entry was not found in the container
+ */
+export default interface NotFoundException extends ContainerException {
+
+}

f74151a02c24c5064ecc42c6d7cbab0f0d61a3dc
Alin Eugen Deac <aedart@gmail.com>
2021-08-24 19:27:10 +0200

    Improve meta type description

--- packages/contracts/src/meta/aliases.ts
+++ packages/contracts/src/meta/aliases.ts
@@ -1,4 +1,8 @@
 /**
- * The type of meta
+ * The type of meta.
+ *
+ * Think of this as a category or
+ * perhaps a tag, which can be used to identify
+ * a specific type of arbitrary data for a target.
  */
 export type MetaType = string | symbol | number;

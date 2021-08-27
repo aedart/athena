49e4d0ad92cf865b2e100bc54c6ed0c1d3ccf547
Alin Eugen Deac <aedart@gmail.com>
2021-08-27 19:07:55 +0200

    Add reflections contracts

--- packages/contracts/rollup.config.js
+++ packages/contracts/rollup.config.js
@@ -73,6 +73,7 @@ export default [
     // Export sub modules
     ...exportSubModule('container'),
     ...exportSubModule('meta'),
+    ...exportSubModule('reflections'),
 
     // Finally, clear the dts directory
     exportClearDtsFiles()

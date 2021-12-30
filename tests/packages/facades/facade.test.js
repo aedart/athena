779c743c9aad4517ef48222680ae689b7e430f5f
Alin Eugen Deac <aedart@gmail.com>
2021-12-30 18:58:26 +0100

    Add tests for facade abstraction and its proxy handler

---
+++ tests/packages/facades/facade.test.js
@@ -0,0 +1,301 @@
+import Container from "@aedart/container";
+import { Facade } from "@aedart/facades";
+import Greeter, { GREETER_IDENTIFIER } from "../../helpers/facades/Greeter";
+import GreeterFacade from "../../helpers/facades/GreeterFacade";
+import faker from 'faker';
+import {fa} from "faker/lib/locales";
+
+fdescribe('@aedart/facades', () => {
+
+    /*****************************************************************
+     * Setup
+     ****************************************************************/
+
+    beforeEach(() => {
+        Facade.serviceContainer = Container.getInstance();
+    });
+
+    afterEach(() => {
+        Facade.clearResolvedInstances();
+        Facade.serviceContainer = null;
+
+        let container = Container.getInstance();
+        container.flush();
+        Container.setInstance(null);
+    });
+
+    /*****************************************************************
+     * Helpers
+     ****************************************************************/
+
+    /**
+     * Registers a binding in the service container,
+     * so that the "dummy" facade's root can work as intended
+     */
+    function bindGreeter() {
+        let container = Container.getInstance();
+
+        container.bind(GREETER_IDENTIFIER, Greeter);
+    }
+
+    /*****************************************************************
+     * Actual Tests
+     ****************************************************************/
+
+    describe('Facade abstraction', () => {
+
+        it('can set and obtain service container', function () {
+            // Reset before attempting to set
+            Facade.serviceContainer = null;
+
+            let dummy = {};
+
+            Facade.serviceContainer = dummy;
+            let result = Facade.serviceContainer;
+
+            // ------------------------------------------------------ //
+
+            expect(result)
+                .withContext('Unable to set and obtain service container')
+                .toBe(dummy);
+
+        });
+
+        it('can obtain facade accessor', function () {
+
+            let expected = 'my-accessor';
+            class DummyFacade extends Facade {
+                facadeAccessor() {
+                    return expected;
+                }
+            }
+
+            let dummy = new DummyFacade();
+            let result = dummy.facadeAccessor();
+
+            // ------------------------------------------------------ //
+
+            expect(result)
+                .withContext('Unable to obtain facade accessor')
+                .toBe(expected);
+        });
+
+        it('can resolve instance from service container', function () {
+            let container = Container.getInstance();
+
+            let abstract = 'my-service';
+            class Service {}
+
+            container.bind(abstract, Service);
+
+            // ------------------------------------------------------ //
+
+            let result = Facade.resolveFacadeInstance(abstract);
+
+            // ------------------------------------------------------ //
+
+            expect(Facade.hasResolvedInstance(abstract))
+                .withContext('Facade does not appear to have resolved instance')
+                .toBeTrue();
+
+            expect(result)
+                .withContext('Unable to resolve instance from service container, using facade')
+                .toBeInstanceOf(Service);
+        });
+
+        it('can clear resolved instance', function () {
+            let container = Container.getInstance();
+
+            let abstract = 'my-service';
+            class Service {}
+
+            container.bind(abstract, Service);
+
+            // ------------------------------------------------------ //
+
+            Facade.resolveFacadeInstance(abstract);
+            let result = Facade.clearResolvedInstance(abstract);
+
+            // ------------------------------------------------------ //
+
+            expect(result)
+                .withContext('It appears facade was unable to clear resolved instance?!?')
+                .toBeTrue();
+
+            expect(Facade.hasResolvedInstance(abstract))
+                .withContext('Resolved instance was NOT cleared')
+                .toBeFalse()
+        });
+    });
+
+    describe('Facade proxy handler', () => {
+
+        it('can obtain facade root', function () {
+            bindGreeter();
+
+            let result = GreeterFacade.facadeRoot();
+
+            // ------------------------------------------------------ //
+
+            expect(result)
+                .withContext('Facade root was not resolved correctly')
+                .toBeInstanceOf(Greeter);
+        });
+
+        it('can proxy properties', function () {
+            bindGreeter();
+
+            let expected = faker.name.findName();
+
+            // ------------------------------------------------------ //
+
+            GreeterFacade.customer = expected;
+            let result = GreeterFacade.customer;
+
+            // ------------------------------------------------------ //
+
+            expect(result)
+                .withContext('Unable to set or obtain property')
+                .toBe(expected);
+        });
+
+        it('can proxy dynamic properties', function () {
+
+            bindGreeter();
+
+            let expected = faker.name.findName();
+            let property = 'foo';
+
+            // ------------------------------------------------------ //
+
+            GreeterFacade[property] = expected;
+            let result = GreeterFacade[property];
+
+            // ------------------------------------------------------ //
+
+            expect(result)
+                .withContext('Unable to set or obtain dynamic property')
+                .toBe(expected);
+        });
+
+        it('can proxy methods', function () {
+            bindGreeter();
+
+            let name = faker.name.findName();
+            let expected = `Welcome ${name}`
+
+            // ------------------------------------------------------ //
+
+            GreeterFacade.customer = name;
+            let result = GreeterFacade.greetings();
+
+            // ------------------------------------------------------ //
+
+            expect(result)
+                .withContext('Method was not invoked correctly')
+                .toBe(expected);
+        });
+
+        it('can proxy methods with arguments', function () {
+            bindGreeter();
+
+            let name = faker.name.findName();
+            let expected = `Welcome ${name}`
+
+            // ------------------------------------------------------ //
+
+            let result = GreeterFacade.sayWelcome(name);
+
+            // ------------------------------------------------------ //
+
+            expect(result)
+                .withContext('Method with args. was not invoked correctly')
+                .toBe(expected);
+        });
+
+        it('can proxy dynamic methods', function () {
+            bindGreeter();
+
+            let expected = 'bar';
+
+            // ------------------------------------------------------ //
+
+            let result = GreeterFacade.foo();
+
+            // ------------------------------------------------------ //
+
+            expect(result)
+                .withContext('Dynamic method not invoked')
+                .toBe(expected);
+        });
+
+        it('can proxy define properties', function () {
+            bindGreeter();
+
+            let property = 'shop';
+            let value = 'Acme Ltd.';
+
+            // ------------------------------------------------------ //
+
+            Object.defineProperty(GreeterFacade, property, {
+                value: value
+            });
+
+            let result = GreeterFacade[property];
+
+            // ------------------------------------------------------ //
+
+            expect(property in GreeterFacade)
+                .withContext('Property was not defined in facade root')
+                .toBeTrue();
+
+            expect(result)
+                .withContext('Incorrect property value defined')
+                .toBe(value);
+        });
+
+        it('can proxy delete properties', function () {
+            bindGreeter();
+
+            let property = 'owner';
+
+            // ------------------------------------------------------ //
+
+            Object.defineProperty(GreeterFacade, property, {
+                value: faker.name.findName(),
+                configurable: true,
+                writable: true
+            });
+
+            delete GreeterFacade[property];
+
+            // ------------------------------------------------------ //
+
+            expect(property in GreeterFacade)
+                .withContext('Property was not deleted in facade root')
+                .toBeFalse()
+        });
+
+        it('can proxy own property descriptor', function () {
+            bindGreeter();
+
+            let name = faker.name.findName();
+            GreeterFacade.customer = name;
+
+            // ------------------------------------------------------ //
+
+            let result = Reflect.getOwnPropertyDescriptor(GreeterFacade, '_customer');
+
+            // ------------------------------------------------------ //
+
+            expect(result)
+                .withContext('No own property descriptor resolved')
+                .not
+                .toBeNull();
+
+            expect(result.value)
+                .withContext('Unexpected property value in descriptor')
+                .toBe(name);
+        });
+    });
+});

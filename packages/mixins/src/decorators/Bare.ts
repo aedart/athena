47182a7adba7fa305ab4353bcb2810ae93141b1e
Alin Eugen Deac <aedart@gmail.com>
2022-01-02 13:50:50 +0100

    Port "bare" decorator

---
+++ packages/mixins/src/decorators/Bare.ts
@@ -0,0 +1,27 @@
+import Wrapper from "../helpers/Wrapper";
+
+/**
+ * Applied mixin symbol
+ */
+export const APPLIED_MIXIN: unique symbol = Symbol('@aedart/mixins/applied-mixin');
+
+/**
+ * Stores reference to current mixin
+ *
+ * @mixin
+ *
+ * @param {Function} mixin
+ *
+ * @return {Function}
+ */
+const Bare = (mixin: Function) => Wrapper.wrap(mixin, (superClass: Function) => {
+    // Apply mixin
+    const applied = mixin(superClass);
+
+    // Store reference to original mixin
+    applied.prototype[APPLIED_MIXIN] = Wrapper.unwrap(mixin);
+
+    return applied;
+});
+
+export default Bare;

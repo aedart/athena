24dbf847f1b9be0fec592f17e52fedc2030eca01
Alin Eugen Deac <aedart@gmail.com>
2022-01-02 09:53:57 +0100

    Fix missing return type JSDoc

--- packages/contracts/src/support/services/Bootable.ts
+++ packages/contracts/src/support/services/Bootable.ts
@@ -5,6 +5,8 @@ export default interface Bootable {
 
     /**
      * Boot the services that are offered by this provider
+     *
+     * @return {PromiseLike<any>|undefined}
      */
     boot(): PromiseLike<any> | undefined;
 }

d646149b93ef32cf4647ff17104db626c99706c1
Alin Eugen Deac <aedart@gmail.com>
2022-01-08 17:24:27 +0100

    Cleanup

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,10 +1,4 @@
 /**
- * The contracts package consists of several "sub" modules.
- *
- * TODO .... a better description ...
- */
-
-/**
  * Contracts package identifier
  */
 const CONTRACTS: unique symbol = Symbol('@aedart/contracts');
